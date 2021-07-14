f91bdb63025bb7b3a60d2cbf144b050133ac25fd
masteryodaa <kujjwal42@gmail.com>
2021-07-14 16:44:30 +0530

    flip

---
+++ flip.js
@@ -0,0 +1,12 @@
+function flip1(){
+    document.querySelector(".card1-inner").classList.toggle("flipped1");
+}
+
+function flip2(){
+    document.querySelector(".card2-inner").classList.toggle("flipped2");
+}
+
+function flip3(){
+    document.querySelector(".card3-inner").classList.toggle("flipped3");
+}
+
